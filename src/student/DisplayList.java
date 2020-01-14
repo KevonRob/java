a885c1161c696d9ce03542d3bd65e3c53f9fa303
kevon <kevon@LAPTOP-G10U1NON>
2020-01-14 13:39:28 -0500

    First commit

---
+++ src/student/DisplayList.java
@@ -0,0 +1,22 @@
+package student;
+
+import java.util.Scanner;
+
+public class DisplayList {
+
+    public static void main(String[] args) {
+        Student[] s = new Student[5];
+        Scanner input = new Scanner(System.in);
+    
+        for (int i=0; i<s.length; i++)
+    {
+        s[i] = new Student();
+        System.out.println("Enter id " + (i+1));
+        s[i].setId(input.nextInt());
+        
+       System.out.println("Enter name " + (i+1));
+        s[i].setName(input.nextLine()); 
+    }
+
+    }
+}
---
+++ src/student/Student.java
@@ -0,0 +1,34 @@
+package student;
+
+public class Student {
+    private String name;
+    private int id;
+    
+    public String getName()
+    {
+        return name;
+    }
+    
+    public int getId()
+    {
+        return id;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+   
+
+}
+
